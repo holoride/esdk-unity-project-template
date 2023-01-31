cb4ba8fd8207acd1747ffc15e9c1b7f20b3a2329
Gerrit Lochmann <gerrit@holoride.com>
2023-01-31 16:47:55 +0100

    added scene selector

---
+++ Assets/2D User Interface/Scripts/Menu.cs
@@ -0,0 +1,84 @@
+// Copyright (c) holoride GmbH. All Rights Reserved.
+
+namespace Holoride.ElasticSDKTemplate
+{
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    public class Menu : MonoBehaviour
+    {
+        [SerializeField] 
+        private GameObject buttonPrefab;
+        
+        [SerializeField] 
+        private ScrollRect scrollRect;
+
+        private readonly float adjustSeconds = 0.2f;
+
+        private RectTransform contentRectTransform;
+        private MenuButton lastSelected;
+        private Vector2 targetScrollPosition;
+        private Vector2 targetScrollVelocity;
+        private Vector2 previousAnchoredPosition;
+
+        public MenuButton LastSelected => this.lastSelected;
+
+        private void Awake()
+        {
+            this.contentRectTransform = this.scrollRect.content.GetComponent<RectTransform>();
+        }
+
+        public Button AddButton(string text, UnityAction action)
+        {
+            var buttonGameObject = GameObject.Instantiate(this.buttonPrefab, this.scrollRect.content.transform);
+
+            var button = buttonGameObject.GetComponent<MenuButton>();
+            var buttonRectTransform = button.GetComponent<RectTransform>();
+            button.onClick.AddListener(action);
+            button.OnSelected.AddListener(() => this.lastSelected = button);
+            button.OnSelected.AddListener(() => this.SnapTo(buttonRectTransform));
+
+            var buttonText = buttonGameObject.GetComponentInChildren<TMP_Text>();
+            buttonText.text = text;
+
+            return button;
+        }
+
+        public void RestorePreviousSelection()
+        {
+            if (this.scrollRect.content.transform.childCount > 0)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+                EventSystem.current.SetSelectedGameObject(this.lastSelected == null
+                    ? this.scrollRect.content.transform.GetChild(0).gameObject
+                    : this.lastSelected.gameObject);
+            }
+        }
+
+        private void OnEnable()
+        {
+            this.RestorePreviousSelection();
+        }
+
+        public void SnapTo(RectTransform target)
+        {
+            Vector2 viewportLocalPosition = this.scrollRect.viewport.localPosition;
+            Vector2 childLocalPosition = target.localPosition;
+            Vector2 result = new Vector2(0, -viewportLocalPosition.y - childLocalPosition.y);
+
+            this.targetScrollPosition = result;
+        }
+
+        private void Update()
+        {
+            this.contentRectTransform.anchoredPosition = Vector2.SmoothDamp(
+                this.contentRectTransform.anchoredPosition,
+                this.targetScrollPosition,
+                ref targetScrollVelocity,
+                this.adjustSeconds);
+        }
+    }
+}
