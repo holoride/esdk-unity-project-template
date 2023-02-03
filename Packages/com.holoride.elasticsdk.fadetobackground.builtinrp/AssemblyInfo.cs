fdb23461a6b2f74e4a27eea8733f96734f37841a
Gerrit Lochmann <gerrit@holoride.com>
2023-02-03 13:36:58 +0100

    cleanup

--- Packages/com.holoride.elasticsdk.fadetobackground.builtinrp/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿// Copyright (c) holoride GmbH. All Rights Reserved.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Holoride.ElasticSDK.FadeToBackground.BuildInRenderPipeline.Editor")]
