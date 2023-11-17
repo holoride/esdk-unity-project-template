5b6a107b4039d27bbc7e2cc62e39e7b66b312cae
Winfried Baumann <frieder@holoride.com>
2023-11-17 14:30:45 +0100

    - moved scripts to common scripts folder - moved changing the level to a separate script and added a description - updated prefabs

---
+++ Assets/2D User Interface/Scripts/LogoAnimator.cs
@@ -0,0 +1,65 @@
+// Copyright (c) holoride GmbH. All Rights Reserved.
+
+namespace Holoride.ElasticSDKTemplate
+{
+    using System;
+    using System.Collections.Generic;
+    using Cysharp.Threading.Tasks;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class LogoAnimator : MonoBehaviour
+    {
+        [SerializeField] private List<CanvasGroup> SplashScreenDisplayGroup;
+
+        [SerializeField] private float SecondsPerLogo = 1f;
+
+        [SerializeField] private AnimationCurve FadeInCurve = AnimationCurve.EaseInOut(0f, 0f, 0.5f, 1f);
+        [SerializeField] private AnimationCurve FadeOutCurve = AnimationCurve.EaseInOut(0f, 1f, 0.5f, 0f);
+
+        [SerializeField] public UnityEvent OnSplashScreensShown;
+
+        private void Start()
+        {
+            foreach (var group in SplashScreenDisplayGroup)
+            {
+                group.alpha = 0f;
+            }
+
+            PlaySplashScreenAnimations().Forget();
+        }
+
+        private async UniTaskVoid PlaySplashScreenAnimations()
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(2f));
+
+            foreach (var group in SplashScreenDisplayGroup)
+            {
+                await AnimateLogo(group);
+            }
+
+            OnSplashScreensShown?.Invoke();
+        }
+
+        private async UniTask AnimateLogo(CanvasGroup group)
+        {
+            var time = 0f;
+            while (group.alpha < 1f)
+            {
+                time += Time.deltaTime;
+                group.alpha = FadeInCurve.Evaluate(time);
+                await UniTask.NextFrame();
+            }
+
+            await UniTask.Delay(TimeSpan.FromSeconds(SecondsPerLogo));
+
+            time = 0f;
+            while (group.alpha > 0f)
+            {
+                time += Time.deltaTime;
+                group.alpha = FadeOutCurve.Evaluate(time);
+                await UniTask.NextFrame();
+            }
+        }
+    }
+}
